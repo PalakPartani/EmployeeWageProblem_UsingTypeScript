5b9fa87da30fd4827985f807f766af46c5cc8b7e
PalakPartani <partanipalak16@gmail.com>
2020-08-26 07:13:17 -0700

    ADDED:Base Employee Wage Project

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('Hello world');
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+empCheck == 1 ? console.log("Employee is present") : console.log("Employee is absent");
+//# sourceMappingURL=app.js.map
