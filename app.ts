cf201bd9af8abab61072bfb819269d3b1bf3c615
PalakPartani <partanipalak16@gmail.com>
2020-08-26 07:16:54 -0700

    ADDED:Feature to check employee is present or absent using Random

--- app.ts
+++ app.ts
@@ -1,1 +1,3 @@
-console.log("Hello");
+let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+empCheck == 1 ? console.log("Employee is present") : console.log("Employee is absent");
